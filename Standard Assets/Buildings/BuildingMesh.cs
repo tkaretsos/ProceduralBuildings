5d315edd3fe27b584ef86caf4f643b59f1b56235
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-28 22:28:56 +0200

    update BuildingMesh class
    - implemented ConstructFaces method
    - added Draw method to render the procedural content

--- Standard Assets/Buildings/BuildingMesh.cs
+++ Standard Assets/Buildings/BuildingMesh.cs
@@ -8,7 +8,7 @@ public class BuildingMesh
 	private float _height;
 	private float _floor_height;
 	private int _floor_number;
-	private BuildingType _type;
+	private readonly BuildingType _type;
 	private List<Vector3> _boundaries = new List<Vector3>();
 	private List<Face> _faces = new List<Face>();
 #pragma warning restore 0414
@@ -60,7 +60,8 @@ public class BuildingMesh
 		_boundaries.Add(p4);
 		_type = type;
 		
-		_height = 0f;
+		//TODO set height to 0 when neoclassical is implemented
+		_height = 6f;
 		_floor_height = 0f;
 		_floor_number = 0;
 	}
@@ -69,6 +70,27 @@ public class BuildingMesh
 	// methods
 	public void ConstructFaces()
 	{
-		//TODO implement after implementing Face class
-	}	
+		_faces.Add(new Face(this, _boundaries[0], _boundaries[1]));
+		_faces.Add(new Face(this, _boundaries[1], _boundaries[2]));
+		_faces.Add(new Face(this, _boundaries[2], _boundaries[3]));
+		_faces.Add(new Face(this, _boundaries[3], _boundaries[0]));
+	}
+	
+	
+	public void Draw(Material mat)
+	{
+		mat.SetPass(0);
+	
+		// draw roof
+		GL.PushMatrix();
+		GL.Begin(GL.QUADS);
+		foreach (Vector3 v in _boundaries)
+			GL.Vertex(v + (new Vector3(0, _height, 0)));
+		GL.End();
+		GL.PopMatrix();	
+		
+		// draw faces
+		foreach (Face face in _faces)
+			face.Draw();
+	}
 }
