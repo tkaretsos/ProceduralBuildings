90a97503b7a12edf76e0d4fe9a200607c82dcc20
Thanasis Karetsos <karetsosjr@gmail.com>
2012-02-08 22:56:33 +0200

    refactor the code that generates FaceComponents in Neoclassical

--- Standard Assets/Buildings/Neoclassical.cs
+++ Standard Assets/Buildings/Neoclassical.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
-using System;
 
-public class Neoclassical : Building
+using Exception = System.Exception;
+
+public sealed class Neoclassical : Building
 {
 	// fields
-	private const float _window_width_min = 1.5f;
-	private const float _window_width_max = 1.75f;
+	private const float _component_width_min = 1.5f;
+	private const float _component_width_max = 1.75f;
 	
-	private const float _inbetween_space_min = 2f;
-	private const float _inbetween_space_max = 2.25f;
+	private const float _component_space_min = 2f;
+	private const float _component_space_max = 2.25f;
 	
 	
 	// constructors
@@ -33,9 +34,8 @@ public class Neoclassical : Building
 	public Neoclassical (Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
 	: base(p1, p2, p3, p4, BuildingType.Neoclassical)
 	{
-		this.FloorHeight = UnityEngine.Random.Range(4f, 5f);
-//		this.FloorNumber = Util.RollDice(new float[] {0.15f, 0.7f, 0.15f});
-		this.FloorNumber = 1;
+		this.FloorHeight = Random.Range(4f, 5f);
+		this.FloorNumber = Util.RollDice(new float[] {0.15f, 0.7f, 0.15f});
 	}
 	
 	
@@ -49,40 +49,51 @@ public class Neoclassical : Building
 		base.ConstructFaces();
 	}
 	
-	public void ConstructFaceComponents ()
+	public override void ConstructFaceComponents ()
 	{
 		if (Faces.Count == 0) throw new Exception("There are no faces to construct the components.");
 		
-		float component_width = UnityEngine.Random.Range(_window_width_min, _window_width_max);
-		float inbetween_space = UnityEngine.Random.Range(_inbetween_space_min, _inbetween_space_max);
-		
-		float offset = 0f;
-		Vector3 dr;
-		Vector3 dl;
+		float component_width = Random.Range(_component_width_min, _component_width_max);
+		float inbetween_space = Random.Range(_component_space_min, _component_space_max);
 		
 		foreach (Face face in Faces)
 		{
 			int components_no = GetComponentsNumber(face.Width, component_width, inbetween_space);
-			
-			// ground floor
-			float effective_width = components_no * component_width + (components_no - 1) * inbetween_space;
-			float distance_from_edge = (face.Width - effective_width) / 2;
-			
-			offset = distance_from_edge;
-			
-			for (int i = 0; i < components_no; ++i)
+			float effective_width = components_no * component_width;
+			float fixed_space = (face.Width - effective_width) / (components_no + 1);
+
+			for (int floor = 0; floor < this.FloorNumber; ++floor)
 			{
-				dr = face.Boundaries[0] - face.Right * offset;
-				dl = dr - face.Right * component_width;
-				offset += component_width;
-				face.AddFaceComponent(new FaceComponent(face, component_width, FloorHeight * 3 / 5, dr, dl));
-				offset += inbetween_space;
+				float offset = fixed_space;
+				for (int i = 0; i < components_no; ++i)
+				{
+					Vector3 dr = face.Boundaries[0] - face.Right * offset + (new Vector3(0f, floor * this.FloorHeight, 0f));
+          Vector3 dl = dr - face.Right * component_width;
+					offset += component_width;
+					face.AddFaceComponent(new FaceComponent(face, component_width, FloorHeight * 3 / 5, dr, dl));
+					offset += fixed_space;
+				}
 			}
 		}
 	}
 	
-	private int GetComponentsNumber (float face_width, float comp_width, float space)
+	/// <summary>
+	/// Calculates the number of components that fit in a face.
+	/// </summary>
+	/// <returns>
+	/// The number of components.
+	/// </returns>
+	/// <param name='face_width'>
+	/// The width of the face.
+	/// </param>
+	/// <param name='component_width'>
+	/// The width of the face components.
+	/// </param>
+	/// <param name='space'>
+	/// The wanted space between each face component, approximately.
+	/// </param>
+	private int GetComponentsNumber (float face_width, float component_width, float space)
 	{
-		return Mathf.CeilToInt((face_width - 2f) / (comp_width + space));
+		return Mathf.CeilToInt(face_width / (component_width + space));
 	}
 }
