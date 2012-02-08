e4f2e90cdce6c1eb1d8f29ac2f442658f255c8a6
Thanasis Karetsos <karetsosjr@gmail.com>
2012-02-08 22:53:45 +0200

    add key control to generate new buildings

--- Scripts/CameraControl.cs
+++ Scripts/CameraControl.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 public class CameraControl : MonoBehaviour 
 {	
@@ -13,16 +12,16 @@ public class CameraControl : MonoBehaviour
 	private CameraMode _cameraMode = CameraMode.Horizontal;
 	
 	[SerializeField]
-	private float _movementSpeed = 10.0f;
+	private float _movementSpeed = 10f;
 	
 	[SerializeField]
-	private float _rotationSpeed = 3.0f;
+	private float _rotationSpeed = 3f;
 	
 	[SerializeField]
 	private Material _primitiveMaterial;
 	
-	private float _x_rotation = 0.0f;
-	private float _y_rotation = 0.0f;
+	private float _x_rotation;
+	private float _y_rotation;
 	
 	private float _last_mouse_x;
 	private float _last_mouse_y;
@@ -34,12 +33,15 @@ public class CameraControl : MonoBehaviour
 	
 	void Start ()
 	{
-		mesh = new Neoclassical(new Vector3( 4f + Random.Range(0.5f, 1.5f), 0,  1.5f + Random.Range(0.5f, 1.5f)),
-								new Vector3( 4f + Random.Range(0.5f, 1.5f), 0, -1.5f - Random.Range(0.5f, 1.5f)),
-								new Vector3(-4f - Random.Range(0.5f, 1.5f), 0, -1.5f - Random.Range(0.5f, 1.5f)), 
-								new Vector3(-4f - Random.Range(0.5f, 1.5f), 0,  1.5f + Random.Range(0.5f, 1.5f)));
+		mesh = new Neoclassical(new Vector3( 4f + Random.Range(0.5f, 1.5f), 0f,  1.5f + Random.Range(0.5f, 1.5f)),
+														new Vector3( 4f + Random.Range(0.5f, 1.5f), 0f, -1.5f - Random.Range(0.5f, 1.5f)),
+														new Vector3(-4f - Random.Range(0.5f, 1.5f), 0f, -1.5f - Random.Range(0.5f, 1.5f)),
+														new Vector3(-4f - Random.Range(0.5f, 1.5f), 0f,  1.5f + Random.Range(0.5f, 1.5f)));
 		mesh.ConstructFaces();
 		mesh.ConstructFaceComponents();
+
+		_x_rotation = -40f;
+		_y_rotation = 50f;
 		
 		_last_mouse_x = Input.GetAxis("Mouse X");
 		_last_mouse_y = Input.GetAxis("Mouse Y");
@@ -68,27 +70,27 @@ public class CameraControl : MonoBehaviour
 		_last_mouse_y = _mouse_y;
 		
 		ClampCamera();
-		camera.transform.eulerAngles = new Vector3(-_x_rotation, _y_rotation, 0.0f);
+		camera.transform.eulerAngles = new Vector3(-_x_rotation, _y_rotation, 0f);
 //		camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation,
 //													 Quaternion.Euler(new Vector3(-_x_rotation, _y_rotation, 0.0f)),
 //													 Time.time);
 		
-		if (Input.GetAxis("Vertical") != 0.0f)
+		if (Input.GetAxis("Vertical") != 0f)
 		{
 			if (_cameraMode == CameraMode.Horizontal)
 				camera.transform.position += Vector3.Cross(camera.transform.right, Vector3.up) *
-											 Input.GetAxis("Vertical") *
-											 _movementSpeed * Time.deltaTime;
+																		 Input.GetAxis("Vertical") *
+																		 _movementSpeed * Time.deltaTime;
 			else
 				camera.transform.position += camera.transform.forward *
-											 Input.GetAxis("Vertical") *
-											 _movementSpeed * Time.deltaTime;
+																		 Input.GetAxis("Vertical") *
+																		 _movementSpeed * Time.deltaTime;
 		}
 		
-		if (Input.GetAxis("Horizontal") != 0.0f)
+		if (Input.GetAxis("Horizontal") != 0f)
 			camera.transform.position += camera.transform.right *
-										 Input.GetAxis("Horizontal") *
-										 _movementSpeed * Time.deltaTime;
+																	 Input.GetAxis("Horizontal") *
+																	 _movementSpeed * Time.deltaTime;
 
 		if (Input.GetKey(KeyCode.E))
 			camera.transform.position += Vector3.up * _movementSpeed * Time.deltaTime;
@@ -102,18 +104,26 @@ public class CameraControl : MonoBehaviour
 			else
 				_cameraMode = CameraMode.Free;
 		
-		if (Input.GetKeyUp(KeyCode.R))
-			Debug.Log(Util.RollDice(new float[] {0.1f, 0.2f, 0.7f}, new int[] {4, 11, 20}));
-		
-		
+//		if (Input.GetKeyUp(KeyCode.R))
+//			Debug.Log(Util.RollDice(new float[] {0.1f, 0.2f, 0.7f}, new int[] {4, 11, 20}));
+
+		if (Input.GetKeyUp(KeyCode.B))
+		{
+			mesh = new Neoclassical(new Vector3( 4f + Random.Range(0.5f, 1.5f), 0f,  1.5f + Random.Range(0.5f, 1.5f)),
+															new Vector3( 4f + Random.Range(0.5f, 1.5f), 0f, -1.5f - Random.Range(0.5f, 1.5f)),
+															new Vector3(-4f - Random.Range(0.5f, 1.5f), 0f, -1.5f - Random.Range(0.5f, 1.5f)),
+															new Vector3(-4f - Random.Range(0.5f, 1.5f), 0f,  1.5f + Random.Range(0.5f, 1.5f)));
+			mesh.ConstructFaces();
+			mesh.ConstructFaceComponents();
+		}
 	}
 	
-	private void ClampCamera ()
+	private void ClampCamera (float horizontal = 360f, float vertical = 80f)
 	{
-		if (_y_rotation < -360.0f) _y_rotation += 360.0f;
-		if (_y_rotation >  360.0f) _y_rotation -= 360.0f;
+		if (_y_rotation < -horizontal) _y_rotation += horizontal;
+		if (_y_rotation >  horizontal) _y_rotation -= horizontal;
 
-        if (_x_rotation >  80.0f) _x_rotation = 80.0f;
-        if (_x_rotation < -80.0f) _x_rotation = -80.0f;
+    if (_x_rotation >  vertical) _x_rotation =  vertical;
+    if (_x_rotation < -vertical) _x_rotation = -vertical;
 	}
 }
