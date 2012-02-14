7c2139cb7a18419ad0c9f8441afbeb44c8ed7cf3
Thanasis Karetsos <karetsosjr@gmail.com>
2012-02-14 20:42:53 +0200

    improve NeoclassicalWindow ctor

--- Standard Assets/Buildings/Neoclassical/NeoclassicalWindow.cs
+++ Standard Assets/Buildings/Neoclassical/NeoclassicalWindow.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class NeoclassicalWindow : FaceComponent {
 
   public NeoclassicalWindow (Face parent, Vector3 dr, Vector3 dl)
-  : base (parent, dr, dl, 7f / 10f)
-  {}
+  : base (parent)
+  {
+    _height = parentFace.parentBuilding.floorHeight * 0.5f;
+
+    float height_modifier = 0.25f * parentFace.parentBuilding.floorHeight;
+
+    _boundaries.Add(new Vector3(dr.x, dr.y + height_modifier, dr.z));
+    _boundaries.Add(new Vector3(dl.x, dl.y + height_modifier, dl.z));
+    _boundaries.Add(new Vector3(dl.x, dl.y + _height + height_modifier, dl.z));
+    _boundaries.Add(new Vector3(dr.x, dr.y + _height + height_modifier, dr.z));
+  }
 }
