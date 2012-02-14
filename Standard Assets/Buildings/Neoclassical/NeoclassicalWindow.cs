eb518f2af3bf485b343ec596826dee17562cf636
Thanasis Karetsos <karetsosjr@gmail.com>
2012-02-14 18:42:29 +0200

    add NeoclassicalWindow

---
+++ Standard Assets/Buildings/Neoclassical/NeoclassicalWindow.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class NeoclassicalWindow : FaceComponent {
+
+  public NeoclassicalWindow (Face parent, Vector3 dr, Vector3 dl)
+  : base (parent, dr, dl, 7f / 10f)
+  {}
+}
