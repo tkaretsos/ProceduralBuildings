0b2439a432326aa54d71ea642ff5f5eb62ac43b1
Thanasis Karetsos <karetsosjr@gmail.com>
2012-04-15 18:23:46 +0300

    add utility function for merging meshes

--- Standard Assets/Util.cs
+++ Standard Assets/Util.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 using Random = System.Random;
 using Exception = System.Exception;
+using CombinablesCollection = System.Collections.Generic.IList<ICombinable>;
 
 public class Util
 {
@@ -59,6 +60,41 @@ public class Util
     }
 
     return expanded[random.Next(precision)];
+  }
+
+  public static GameObject CombineMeshes (string name,
+                                          string materialName,
+                                          CombinablesCollection objects,
+                                          GameObject parent = null)
+  {
+    var gameObject = new GameObject(name);
+    gameObject.active = false;
+    if (parent != null)
+      gameObject.transform.parent = parent.transform;
+    var meshFilter = gameObject.AddComponent<MeshFilter>();
+    var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+    meshRenderer.sharedMaterial = Resources.Load("Materials/" + materialName,
+                                                 typeof(Material)) as Material;
+
+
+    MeshFilter[] meshFilters = new MeshFilter[objects.Count];
+    for (var i = 0; i < objects.Count; ++i)
+    {
+      meshFilters[i] = objects[i].meshFilter;
+      GameObject.Destroy(objects[i].gameObject);
+    }
+
+    CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+    for (var i = 0; i < meshFilters.Length; ++i)
+    {
+      combine[i].mesh = meshFilters[i].sharedMesh;
+      combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+    }
+
+    meshFilter.mesh = new Mesh();
+    meshFilter.mesh.CombineMeshes(combine);
+
+    return gameObject;
   }
 
 	
