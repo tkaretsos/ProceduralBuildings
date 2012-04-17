2b2b2553acc200e91ad3f740fb05b9d450482bc0
Thanasis Karetsos <karetsosjr@gmail.com>
2012-04-17 15:55:31 +0300

    BalconyFloor now implements IDrawable

--- Standard Assets/Base/BalconyFloor.cs
+++ Standard Assets/Base/BalconyFloor.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+using IDrawable = Thesis.Interface.IDrawable;
+
 namespace Thesis {
 namespace Base {
 
-public class BalconyFloor : Drawable
+public class BalconyFloor : IDrawable
 {
   /*************** FIELDS ***************/
 
@@ -23,10 +25,33 @@ public class BalconyFloor : Drawable
     get { return (Neoclassical) parentBalconyDoor.parentBuilding; }
   }
 
+  private Vector3[] _vertices;
+  public Vector3[] vertices
+  {
+    get { return _vertices; }
+  }
+
+  private int[] _triangles;
+  public int[] triangles
+  {
+    get { return _triangles; }
+  }
+
+  private GameObject _gameObject;
+  public GameObject gameObject
+  {
+    get { return _gameObject; }
+  }
+
+  private MeshFilter _meshFilter;
+  public MeshFilter meshFilter
+  {
+    get { return _meshFilter; }
+  }
+
   /*************** CONSTRUCTORS ***************/
 
   public BalconyFloor (BalconyDoor parent)
-    : base ("balcony_floor", "Building")
   {
     parentBalconyDoor = parent;
 
@@ -36,24 +61,24 @@ public class BalconyFloor : Drawable
 
     var tmp_right = parentBalconyDoor.parentFace.right * width;
     var tmp_normal = parentBalconyDoor.parentFace.normal * depth;
-    
-    boundaries.Add(parentBalconyDoor.boundaries[1] - tmp_right + tmp_normal + parentBuilding.meshOrigin);
-    boundaries.Add(parentBalconyDoor.boundaries[1] - tmp_right - tmp_normal + parentBuilding.meshOrigin);
-    boundaries.Add(parentBalconyDoor.boundaries[0] + tmp_right - tmp_normal + parentBuilding.meshOrigin);
-    boundaries.Add(parentBalconyDoor.boundaries[0] + tmp_right + tmp_normal + parentBuilding.meshOrigin);
+
+    boundaries.Add(parentBalconyDoor.boundaries[1] - tmp_right + tmp_normal);
+    boundaries.Add(parentBalconyDoor.boundaries[1] - tmp_right - tmp_normal);
+    boundaries.Add(parentBalconyDoor.boundaries[0] + tmp_right - tmp_normal);
+    boundaries.Add(parentBalconyDoor.boundaries[0] + tmp_right + tmp_normal);
 
     for (var i = 0; i < 4; ++i)
       boundaries.Add(boundaries[i] + Vector3.up * height);
   }
 
-  public override Vector3[] FindVertices ()
+  public virtual void FindVertices ()
   {
-    return boundaries.ToArray();
+    _vertices = boundaries.ToArray();
   }
 
-  public override int[] FindTriangles ()
+  public virtual void FindTriangles ()
   {
-    return new int[] {
+    _triangles = new int[] {
       // bottom
       0, 2, 1,
       0, 3, 2,
@@ -74,6 +99,35 @@ public class BalconyFloor : Drawable
       3, 7, 6,
       3, 6, 2
     };
+  }
+
+  public virtual void Draw ()
+  {
+    _gameObject = new GameObject("balcony_floor");
+    _gameObject.transform.parent = parentBuilding.gameObject.transform;
+    _gameObject.isStatic = true;
+    var meshRenderer = _gameObject.AddComponent<MeshRenderer>();
+    _meshFilter = _gameObject.AddComponent<MeshFilter>();
+    _gameObject.active = false;
+    _gameObject.transform.position = parentBuilding.meshOrigin;
+
+    meshRenderer.sharedMaterial = Resources.Load("Materials/Building", typeof(Material)) as Material;
+
+    var mesh = new Mesh();
+    mesh.Clear();
+
+    mesh.vertices = vertices;
+    mesh.triangles = triangles;
+
+    // this is required to stop a runtime warning
+    mesh.uv = new Vector2[mesh.vertices.Length];
+    //for (int i = 0; i < mesh.vertices.Length; ++i)
+    //  mesh.uv[i] = new Vector2(mesh.vertices[i].x, mesh.vertices[i].y);
+
+    mesh.RecalculateNormals();
+    mesh.Optimize();
+
+    _meshFilter.sharedMesh = mesh;
   }
 }
 
