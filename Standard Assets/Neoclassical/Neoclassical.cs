a9e333cac7a840c8a4be1abcccaef7077c927bef
Thanasis Karetsos <karetsosjr@gmail.com>
2012-04-26 20:16:14 +0300

    refactor Neoclassical to Building

--- Standard Assets/Neoclassical/Neoclassical.cs
+++ Standard Assets/Neoclassical/Neoclassical.cs
@@ -1,40 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
-
-using CombinablesCollection = System.Collections.Generic.IList<Thesis.Interface.ICombinable>;
 
 namespace Thesis {
 
-public class Neoclassical
+public class Neoclassical : Base.Building
 {
-  /*************** FIELDS ***************/
-  
-  public GameObject gameObject;
-
-  public NeoBuildingMesh buildingMesh;
-
-  private Dictionary<string, CombinablesCollection> _combinables;
-
   /*************** CONSTRUCTORS ***************/
 
   public Neoclassical (Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+    : base()
   {
-    _combinables = new Dictionary<string, CombinablesCollection>();
     buildingMesh = new NeoBuildingMesh(this, p1, p2, p3, p4);
     gameObject = new GameObject("Neoclassical");
     gameObject.transform.position = buildingMesh.meshOrigin;
-  }
-
-  public void AddCombinable(string materialName, Interface.ICombinable combinable)
-  {
-    if (!_combinables.ContainsKey(materialName))
-    {
-      CombinablesCollection temp = new List<Interface.ICombinable>();
-      temp.Add(combinable);
-      _combinables.Add(materialName, temp);
-    }
-    else
-      _combinables[materialName].Add(combinable);
   }
 }
 
