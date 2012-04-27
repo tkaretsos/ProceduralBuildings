0cf9908ef7215f16085f77d25374fcd0b818bed6
Thanasis Karetsos <karetsosjr@gmail.com>
2012-04-27 20:41:34 +0300

    add TextureManager

---
+++ Standard Assets/TextureManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Thesis {
+
+public sealed class TextureManager
+{
+  private static readonly TextureManager _instance = new TextureManager();
+  public static TextureManager Instance
+  {
+    get { return _instance; }
+  }
+  
+  private Dictionary<string, ProceduralTexture> _textures;
+
+  private TextureManager ()
+  {
+    _textures = new Dictionary<string, ProceduralTexture>();
+  }
+
+  public void Add (string name, Type type)
+  {
+    if (!_textures.ContainsKey(name))
+    {
+      ConstructorInfo[] ctors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+      var texture = (ProceduralTexture) ctors[0].Invoke(new object[] { 2f, 1f });
+      texture.Draw();
+      _textures.Add(name, texture);
+    }
+  }
+
+  public ProceduralTexture Get (string name)
+  {
+    return _textures.ContainsKey(name) ? _textures[name] : null;
+  }
+}
+
+} // namespace Thesis
