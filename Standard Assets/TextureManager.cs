91dacd3c9bdd92a215a87584b6925d9af3e231d2
Thanasis Karetsos <karetsosjr@gmail.com>
2012-05-01 21:36:05 +0300

    add collections

--- Standard Assets/TextureManager.cs
+++ Standard Assets/TextureManager.cs
@@ -15,6 +15,8 @@ public sealed class TextureManager
   
   private Dictionary<string, ProceduralTexture> _textures;
 
+  private Dictionary<string, List<ProceduralTexture>> _collections;
+
   private TextureManager ()
   {
     _textures = new Dictionary<string, ProceduralTexture>();
@@ -26,6 +28,18 @@ public sealed class TextureManager
       _textures.Add(name, texture);
   }
 
+  public void AddToCollection (string name, ProceduralTexture texture)
+  {
+    if (_collections.ContainsKey(name))
+      _collections[name].Add(texture);
+    else
+    {
+      var list = new List<ProceduralTexture>();
+      list.Add(texture);
+      _collections.Add(name, list);
+    }
+  }
+
   public void Set (string name, ProceduralTexture texture)
   {
     if (_textures.ContainsKey(name))
@@ -35,6 +49,11 @@ public sealed class TextureManager
   public ProceduralTexture Get (string name)
   {
     return _textures.ContainsKey(name) ? _textures[name] : null;
+  }
+
+  public List<ProceduralTexture> GetCollection (string name)
+  {
+    return _collections.ContainsKey(name) ? _collections[name] : null;
   }
 }
 
