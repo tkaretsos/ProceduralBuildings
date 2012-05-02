648dd81f1d8c82ec15b1ac7b0f96908bd20f9e78
Thanasis Karetsos <karetsosjr@gmail.com>
2012-05-02 11:26:29 +0300

    add windowMaterial field

--- Standard Assets/Neoclassical/Neoclassical.cs
+++ Standard Assets/Neoclassical/Neoclassical.cs
@@ -4,6 +4,8 @@ namespace Thesis {
 
 public class Neoclassical : Base.Building
 {
+  public readonly Material windowMaterial;
+
   /*************** CONSTRUCTORS ***************/
 
   public Neoclassical (Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
@@ -12,6 +14,10 @@ public class Neoclassical : Base.Building
     buildingMesh = new NeoBuildingMesh(this, p1, p2, p3, p4);
     gameObject = new GameObject("Neoclassical");
     gameObject.transform.position = buildingMesh.meshOrigin;
+
+    // find window material randomly
+    var list = MaterialManager.Instance.GetCollection("mat_neo_window");
+    windowMaterial = list[Random.Range(0, list.Count)];
   }
 }
 
