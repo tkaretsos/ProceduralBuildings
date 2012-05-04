fc4ee2154d46c397867ea3dfe7e8783a2d3c1637
Thanasis Karetsos <karetsosjr@gmail.com>
2012-05-04 20:59:54 +0300

    add and use doorMaterial field

--- Standard Assets/Neoclassical/Neoclassical.cs
+++ Standard Assets/Neoclassical/Neoclassical.cs
@@ -9,6 +9,8 @@ public class Neoclassical : Base.Building
 
   public readonly Material balconyDoorMaterial;
 
+  public readonly Material doorMaterial;
+
   /*************** CONSTRUCTORS ***************/
 
   public Neoclassical (Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
@@ -19,8 +21,14 @@ public class Neoclassical : Base.Building
     var num = Random.Range(0, list.Count - 1);
     windowMaterial = list[num];
 
+    // balcony door material
     list = MaterialManager.Instance.GetCollection("mat_neo_balcony_door");
     balconyDoorMaterial = list[num];
+
+    // door material
+    list = MaterialManager.Instance.GetCollection("mat_neo_door");
+    num = Random.Range(0, list.Count - 1);
+    doorMaterial = list[num];
 
     // must be _after_ the initialization of this object
     buildingMesh = new NeoBuildingMesh(this, p1, p2, p3, p4);
