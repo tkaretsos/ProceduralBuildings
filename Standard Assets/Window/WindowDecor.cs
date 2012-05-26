8a76a3db928f92d53079915ae991e4cb2adea057
Thanasis Karetsos <karetsosjr@gmail.com>
2012-05-26 21:06:22 +0300

    include bottom side window decoration

--- Standard Assets/Window/WindowDecor.cs
+++ Standard Assets/Window/WindowDecor.cs
@@ -13,13 +13,15 @@ public class ComponentDecor : DrawableObject
     get { return parentComponent.parentBuilding.parent; }
   }
 
+  private readonly bool _parentIsWindow;
+
   public ComponentDecor (FaceComponent parent)
   {
     parentComponent = parent;
-
+    _parentIsWindow = parent.GetType().Equals(typeof(Window)) ? true : false;
     width = 0.3f;
 
-    boundaries = new Vector3[12];
+    boundaries = new Vector3[16];
 
     for (int i = 0; i < 4; ++i)
       boundaries[i] = parentComponent.boundaries[i] +
@@ -36,18 +38,21 @@ public class ComponentDecor : DrawableObject
     boundaries[9]  = boundaries[8] + 2 * width * Vector3.up;
     boundaries[11] = boundaries[3] + 2 * dist;
     boundaries[10] = boundaries[11] + 2 * width * Vector3.up;
+
+    boundaries[12] = boundaries[5] - 4 * width * Vector3.up;
+    boundaries[13] = boundaries[5];
+    boundaries[14] = boundaries[4];
+    boundaries[15] = boundaries[4] - 4 * width * Vector3.up;
   }
 
   public override void FindVertices()
   {
-    vertices = new Vector3[boundaries.Length * 2];
-    for (int i = 0; i < 2; ++i)
-      System.Array.Copy(boundaries, 0, vertices, i * boundaries.Length, boundaries.Length);
+    vertices = boundaries;
   }
 
   public override void FindTriangles()
   {
-    triangles = new int[18];
+    triangles = new int[_parentIsWindow ? 24 : 18];
     int i = 0;
 
     // left side
@@ -76,6 +81,18 @@ public class ComponentDecor : DrawableObject
     triangles[i++] = 8;
     triangles[i++] = 10;
     triangles[i++] = 11;
+
+    // bottom
+    if (_parentIsWindow)
+    {
+      triangles[i++] = 12;
+      triangles[i++] = 13;
+      triangles[i++] = 14;
+
+      triangles[i++] = 12;
+      triangles[i++] = 14;
+      triangles[i++] = 15;
+    }
   }
 
   public override void Draw()
@@ -84,20 +101,32 @@ public class ComponentDecor : DrawableObject
 
     var uvs = new Vector2[mesh.vertices.Length];
 
+    // left
     uvs[5] = new Vector2(  0f, 0f);
-    uvs[1] = new Vector2(.25f, 0f);
+    uvs[1] = new Vector2(.125f, 0f);
     uvs[6] = new Vector2(  0f, 1f);
-    uvs[2] = new Vector2(.25f, 1f);
+    uvs[2] = new Vector2(.125f, 1f);
 
-    uvs[0] = new Vector2(.25f, 0f);
-    uvs[4] = new Vector2( .5f, 0f);
-    uvs[3] = new Vector2(.25f, 1f);
-    uvs[7] = new Vector2( .5f, 1f);
+    // right
+    uvs[0] = new Vector2(.125f, 0f);
+    uvs[4] = new Vector2( .25f, 0f);
+    uvs[3] = new Vector2(.125f, 1f);
+    uvs[7] = new Vector2( .25f, 1f);
 
-    uvs[8] = new Vector2(.5f, 1f);
-    uvs[11] = new Vector2(.5f, 0f);
-    uvs[9] = new Vector2( 1f, 1f);
-    uvs[10] = new Vector2( 1f, 0f);
+    // top
+    uvs[8]  = new Vector2(.25f, 1f);
+    uvs[11] = new Vector2(.25f, 0f);
+    uvs[9]  = new Vector2( .5f, 1f);
+    uvs[10] = new Vector2( .5f, 0f);
+
+    // bottom
+    if (_parentIsWindow)
+    {
+      uvs[15] = new Vector2(.5f, 0f);
+      uvs[14] = new Vector2( 1f, 0f);
+      uvs[12] = new Vector2(.5f, 1f);
+      uvs[13] = new Vector2( 1f, 1f);
+    }
 
     mesh.uv = uvs;
 
