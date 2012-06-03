65224affd68f3e3a4140606bc69c7d654f87e21c
Thanasis Karetsos <karetsosjr@gmail.com>
2012-06-03 20:43:14 +0300

    add Edge class

---
+++ Standard Assets/City/Edge.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Thesis {
+  
+public class Edge
+{
+  private Vector3 _start;
+  public Vector3 start
+  {
+    get { return _start; }
+    set
+    {
+      _start = value;
+      _length = Vector3.Distance(_start, _end);
+      _middle = (_start + _end) / 2;
+      _direction = _end - _start;
+    }
+  }
+
+  private Vector3 _end;
+  public Vector3 end
+  {
+    get { return _end; }
+    set
+    {
+      _end = value;
+      _length = Vector3.Distance(_start, _end);
+      _middle = (_start + _end) / 2;
+      _direction = _end - _start;
+    }
+  }
+
+  private Vector3 _middle;
+  public Vector3 middle
+  {
+    get { return _middle; }
+  }
+
+  private float _length;
+  public float length
+  {
+    get { return _length; }
+  }
+
+  private Vector3 _direction;
+  public Vector3 direction
+  {
+    get { return _direction; }
+  }
+
+  private Edge () { }
+
+  public Edge (Vector3 from, Vector3 to)
+  {
+    _start = from;
+    _end = to;
+    _length = Vector3.Distance(from, to);
+    _middle = (from + to) / 2;
+    _direction = to - from;
+  }
+}
+
+} // namespace Thesis
