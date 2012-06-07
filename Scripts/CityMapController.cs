849d8363a8ae56992cffb64d1d6869672efd2e49
Thanasis Karetsos <karetsosjr@gmail.com>
2012-06-07 12:08:43 +0300

    construct buildings according to citymap

--- Scripts/CityMapController.cs
+++ Scripts/CityMapController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Thesis;
+using System.Linq;
 
 public class CityMapController : MonoBehaviour {
 
@@ -7,20 +8,51 @@ public class CityMapController : MonoBehaviour {
 
   void Awake ()
   {
+    ColorManager.Instance.Init();
+    TextureManager.Instance.Init();
+    MaterialManager.Instance.Init();
+
     // the point Vector3.zero must _not_ be used
     // as starting point and all 4 points must be
     // in the first quadrant
     block = new Block(new Vector3(1f, 0f, 0f),
                       new Vector3(0f, 0f, 300f),
-                      new Vector3(700f, 0f, 300f),
-                      new Vector3(700f, 0f, 0f));
+                      new Vector3(500f, 0f, 300f),
+                      new Vector3(500f, 0f, 0f));
     block.Bisect();
+
+    //AddBuildings();
   }
-	
+
   void Update ()
   {
-    foreach (Block b in CityMapManager.Instance.blocks)
-      foreach (Edge e in b.lot.edges)
-        Debug.DrawLine(e.start, e.end, Color.green);
+    //foreach (Block b in CityMapManager.Instance.blocks)
+    //  foreach (Edge e in b.lot.edges)
+    //    Debug.DrawLine(e.start, e.end, Color.green);
+
+    foreach (BuildingLot b in CityMapManager.Instance.lots)
+      foreach (Edge e in b.edges)
+        Debug.DrawLine(e.start, e.end, Color.cyan);
+
+    if (Input.GetKeyUp(KeyCode.B))
+      AddBuildings();
+  }
+
+  private void AddBuildings ()
+  {
+    Building building;
+
+    foreach (BuildingLot l in CityMapManager.Instance.lots)
+    {
+      building = new Building(l.edges[0].start,
+                              l.edges[1].start,
+                              l.edges[2].start,
+                              l.edges[3].start);
+      building.buildingMesh.FindVertices();
+      building.buildingMesh.FindTriangles();
+      building.buildingMesh.Draw();
+      building.CombineSubmeshes();
+      building.gameObject.SetActiveRecursively(true);
+    }
   }
 }
