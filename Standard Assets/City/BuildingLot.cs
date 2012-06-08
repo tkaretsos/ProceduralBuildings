ba5256460eadf96b9174752a6ad91e28b49a7f52
Thanasis Karetsos <karetsosjr@gmail.com>
2012-06-09 00:45:04 +0300

    BuildingLot overhaul

--- Standard Assets/City/BuildingLot.cs
+++ Standard Assets/City/BuildingLot.cs
@@ -7,104 +7,65 @@ namespace Thesis {
 public sealed class BuildingLot
 {
   // edges[0] is _always_ adjacent to a street
-  public readonly List<Edge> edges;
+  public readonly List<Edge> edges = new List<Edge>();
 
-  public readonly BuildingLot initialLot;
+  // key -> index of edge
+  // value -> percentage of length that is occupied
+  public Dictionary<int, float> occupied;
 
-  private bool _isFinal;
-
-  // edges[_biggest] gives the longest edge
-  public int biggest;
+  // key -> index of edge
+  // value -> list that contains all the points on that edge
+  public Dictionary<int, List<Vector3>> pointsInEdge;
 
   public BuildingLot (Block parent)
   {
-    this.initialLot = this;
-    edges = new List<Edge>();
     Vector3 mod, from, to;
+    var edgeList = new List<Edge>();
     for (int i = 0; i < 4; ++i)
     {
       mod = Vector3.ClampMagnitude(10 * (parent.edges[i].direction - parent.edges[(i + 3) % 4].direction), 6f);
       from = parent.edges[i].start + mod;
       mod = Vector3.ClampMagnitude(10 * (parent.edges[(i + 1) % 4].direction - parent.edges[i].direction), 6f);
       to = parent.edges[i].end + mod;
-      edges.Add(new Edge(from, to));
+      edgeList.Add(new Edge(from, to));
     }
 
-    // find index of longest edge
-    biggest = edges.FindIndex(delegate (Edge edge) {
-      return edge.length == edges.Max(e => e.length);
+    int big = edgeList.FindIndex(delegate (Edge e) {
+      return e.length == edgeList.Max(t => t.length);
     });
+    for (int i = 0; i < 4; ++i)
+      edges.Add(edgeList[(big + i) % 4]);
 
-    FindIfIsFinal();
+    occupied = new Dictionary<int,float>();
+    occupied.Add(0, 0f);
+    occupied.Add(1, 0f);
+    occupied.Add(2, 0f);
+    occupied.Add(3, 0f);
+
+    pointsInEdge = new Dictionary<int,List<Vector3>>();
+    pointsInEdge.Add(0, new List<Vector3>());
+    pointsInEdge.Add(1, new List<Vector3>());
+    pointsInEdge.Add(2, new List<Vector3>());
+    pointsInEdge.Add(3, new List<Vector3>());
   }
 
-  public BuildingLot (Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4, BuildingLot source)
+  public BuildingLot (Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
   {
-    initialLot = source.initialLot;
-    edges = new List<Edge>();
     edges.Add(new Edge(p1, p2));
     edges.Add(new Edge(p2, p3));
     edges.Add(new Edge(p3, p4));
     edges.Add(new Edge(p4, p1));
-
-    // find index of longest edge
-    biggest = edges.FindIndex(delegate (Edge edge) {
-      return edge.length == edges.Max(e => e.length);
-    });
-
-    FindIfIsFinal();
   }
 
-  private void FindIfIsFinal ()
+  public bool isFinal ()
   {
-    float max = edges[0].length;
     float min = edges.Min(e => e.length);
 
-    if (max >= 2.5f * min)
-    {
-      _isFinal = false;
-      return;
-    }
+    if (edges[0].length >= 2.5f * min)
+      return false;
 
-    if (max < 17f && min < 16f)
-    {
-      _isFinal = true;
-      return;
-    }
-
-    if (max < 25f && min < 16f && Util.RollDice(new float[] { 0.2f, 0.8f }) == 1)
-    {
-      _isFinal = true;
-      return;
-    }
-
-    _isFinal = false;
-  }
-
-  public void Bisect ()
-  {
-    if (_isFinal)
-    {
-      if (initialLot.EdgesContain(edges[0].start) || initialLot.EdgesContain(edges[0].end))
-        CityMapManager.Instance.Add(this);
-      return;
-    }
-
-    BuildingLot b1 = new BuildingLot(edges[3].start, edges[3].end,
-                                     edges[0].middle, edges[2].middle,
-                                     this);
-    b1.Bisect();
-    BuildingLot b2 = new BuildingLot(edges[1].start, edges[1].end,
-                                     edges[2].middle, edges[0].middle,
-                                     this);
-    b2.Bisect();
-  }
-
-  public bool EdgesContain (Vector3 point)
-  {
-    foreach (Edge e in edges)
-      if (e.Contains(point))
-        return true;
+    if (edges[0].length < 16f && min < 16f)
+      return true;
 
     return false;
   }
