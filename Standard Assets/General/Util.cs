e425eb96ac6bf596eff3793367fdddff6622ee91
Thanasis Karetsos <karetsosjr@gmail.com>
2012-06-09 00:43:34 +0300

    add IntersectionPoint util function

--- Standard Assets/General/Util.cs
+++ Standard Assets/General/Util.cs
@@ -81,6 +81,24 @@ public class Util
     return gameObject;
   }
 
+  public static Vector3 IntersectionPoint (Vector3 p1, Vector3 dir1,
+                                           Vector3 p2, Vector3 dir2)
+  {
+    var dir_cross = Vector3.Cross(dir1, dir2);
+    var tmp_cross = Vector3.Cross(p2 - p1, dir2);
+
+    float c1;
+    if (dir_cross.x != 0f)
+      c1 = tmp_cross.x / dir_cross.x;
+    else if (dir_cross.y != 0f)
+      c1 = tmp_cross.y / dir_cross.y;
+    else if (dir_cross.z != 0f)
+      c1 = tmp_cross.z / dir_cross.z;
+    else
+      throw new System.DivideByZeroException("Result of cross product is Vector3(0,0,0)!");
+
+    return p1 + c1 * dir1;
+  }
 	
   public static void PrintVector (string s, Vector3 v)
   {
