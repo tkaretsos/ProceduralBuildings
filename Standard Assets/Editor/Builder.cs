ca45a1aecf1c961830800f2a0b8544f5c19d5098
Thanasis Karetsos <karetsosjr@gmail.com>
2012-06-12 21:25:46 +0300

    finished Builder editor

--- Standard Assets/Editor/Builder.cs
+++ Standard Assets/Editor/Builder.cs
@@ -6,8 +6,16 @@ using System;
 
 using Object = UnityEngine.Object;
 
-public class Builder : EditorWindow {
-
+public class Builder : EditorWindow
+{
+  // dimensions stuff
+  private Vector2 _p1 = Vector2.zero;
+  private Vector2 _p2 = Vector2.zero;
+  private Vector2 _p3 = Vector2.zero;
+  private Vector2 _p4 = Vector2.zero;
+  private float _width = 0f, _depth = 0f, _height = 0f;
+  private int _floors = 0;
+  
   private enum Area { noChoice, dimensions, points }
   private Area choice1 = Area.noChoice;
 
@@ -20,16 +28,44 @@ public class Builder : EditorWindow {
   }
   private Heights choice2 = Heights.noChoice;
 
+  // component stuff
+  private bool inputCompCount = false;
+  private int componentCount = 0;
+  private bool windowFoldout = false;
+  private float windowHeight = 0f;
+  private float windowWidth = 0f;
+  private bool setWindowWidth = false;
+  private bool setWindowHeight = false;
+  private bool doorFoldout = false;
+  private float doorHeight = 0f;
+  private float doorWidth = 0f;
+  private bool setDoorWidth = false;
+  private bool setDoorHeight = false;
+  private bool balconyFoldout = false;
+  private bool setBalconyWidth = false;
+  private float balconyWidth = 0f;
+  private bool setBalconyHeight = false;
+  private float balconyHeight = 0f;
+
+  // roof stuff
+  private enum myRType
+  {
+    nothing,
+    Flat,
+    SinglePeak,
+    DoublePeak
+  }
+  private myRType rtype = myRType.nothing;
+  private bool inputRoofDecor = false;
+  private Object roofDecorTexture;
+  private bool inputRoof = false;
+  private Object roofTexture;
+  private bool inputRoofBase = false;
+  private Object roofBaseTexture;
+
   private Building _building = new Building();
   private Object texture;
-
-  private Vector2 _p1 = Vector2.zero;
-  private Vector2 _p2 = Vector2.zero;
-  private Vector2 _p3 = Vector2.zero;
-  private Vector2 _p4 = Vector2.zero;
-  private float _width = 0f, _depth = 0f, _height = 0f;
-  private int _floors = 0;
-
+  
   [MenuItem ("Window/Builder")]
   static void Init ()
   {
@@ -42,8 +78,7 @@ public class Builder : EditorWindow {
 
   void OnGUI ()
   {
-    BuildingDimensions();
-
+    GUILayout.Space(20);
     EditorGUILayout.BeginHorizontal();
       if (GUILayout.Button("Destroy"))
       {
@@ -63,6 +98,13 @@ public class Builder : EditorWindow {
         //_building.Draw();
       }
     EditorGUILayout.EndHorizontal();
+
+    GUILayout.Space(20);
+    BuildingDimensions();
+    GUILayout.Space(10);
+    ComponentParams();
+    GUILayout.Space(10);
+    RoofParams();
   }
 
   void OnDestroy ()
@@ -80,7 +122,7 @@ public class Builder : EditorWindow {
     //Repaint();
   }
   
-  void BuildingDimensions ()
+  private void BuildingDimensions ()
   {
     EditorGUILayout.LabelField("Dimensions", EditorStyles.boldLabel);
 
@@ -145,5 +187,234 @@ public class Builder : EditorWindow {
         _building.floorCount = 0;
         break;
     }
+  }
+
+  private void ComponentParams ()
+  {
+    EditorGUILayout.LabelField("Components", EditorStyles.boldLabel);
+
+    EditorGUILayout.BeginHorizontal();
+    EditorGUILayout.LabelField("Components per floor");
+    inputCompCount = EditorGUILayout.Toggle(inputCompCount);
+    if (inputCompCount)
+    {
+      componentCount = EditorGUILayout.IntField(componentCount, GUILayout.Width(25));
+      _building.componentsPerFloor = componentCount;
+    }
+    else
+    {
+      componentCount = 0;
+      _building.componentsPerFloor = 0;
+    }
+    EditorGUILayout.EndHorizontal();
+
+    //EditorGUILayout.BeginHorizontal();
+    //EditorGUILayout.LabelField("Component distance");
+    //inputCompDistance = EditorGUILayout.Toggle(inputCompDistance);
+    //if (inputCompDistance)
+    //{
+    //  distance = EditorGUILayout.FloatField(distance, GUILayout.Width(25));
+    //  _building.distance = distance;
+    //}
+    //else
+    //{
+    //  distance = 0f;
+    //  _building.distance = 0f;
+    //}
+    //EditorGUILayout.EndHorizontal();
+
+    // Window
+    windowFoldout = EditorGUILayout.Foldout(windowFoldout, "Window");
+    if (windowFoldout)
+    {
+      // width
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Width");
+      setWindowWidth = EditorGUILayout.Toggle(setWindowWidth);
+      if (setWindowWidth)
+        windowWidth = EditorGUILayout.FloatField(windowWidth, GUILayout.Width(30));
+      else
+        windowWidth = 0f;
+      EditorGUILayout.EndHorizontal();
+      _building.windowWidth = windowWidth;
+
+      // height
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Height");
+      setWindowHeight = EditorGUILayout.Toggle(setWindowHeight);
+      if (setWindowHeight)
+        windowHeight = EditorGUILayout.FloatField(windowHeight, GUILayout.Width(30));
+      else
+        windowHeight = 0f;
+      EditorGUILayout.EndHorizontal();
+      _building.windowHeight = windowHeight;
+      
+      // material
+      //EditorGUILayout.BeginHorizontal();
+      //EditorGUILayout.LabelField("Texture");
+      //setWindowTexture = EditorGUILayout.Toggle(setWindowTexture);
+      //if (setWindowTexture)
+      //  windowTexture = EditorGUILayout.ObjectField(windowTexture,
+      //                                              typeof(Texture),
+      //                                              false,
+      //                                              GUILayout.Height(55));
+      //else
+      //  windowTexture = null;
+      //EditorGUILayout.EndHorizontal();
+      // find material by windowtexture name
+    }
+
+    // Door
+    doorFoldout = EditorGUILayout.Foldout(doorFoldout, "Door");
+    if (doorFoldout)
+    {
+      // width
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Width");
+      setDoorWidth = EditorGUILayout.Toggle(setDoorWidth);
+      if (setDoorWidth)
+        doorWidth = EditorGUILayout.FloatField(doorWidth, GUILayout.Width(30));
+      else
+        doorWidth = 0f;
+      EditorGUILayout.EndHorizontal();
+      _building.doorWidth = doorWidth;
+
+      // height
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Height");
+      setDoorHeight = EditorGUILayout.Toggle(setDoorHeight);
+      if (setDoorHeight)
+        doorHeight = EditorGUILayout.FloatField(doorHeight, GUILayout.Width(30));
+      else
+        doorHeight = 0f;
+      EditorGUILayout.EndHorizontal();
+      _building.doorHeight = doorHeight;
+
+      // material
+      //EditorGUILayout.BeginHorizontal();
+      //EditorGUILayout.LabelField("Texture");
+      //setDoorTexture = EditorGUILayout.Toggle(setDoorTexture);
+      //if (setDoorTexture)
+      //  doorTexture = EditorGUILayout.ObjectField(doorTexture,
+      //                                            typeof(Texture),
+      //                                            false,
+      //                                            GUILayout.Height(55));
+      //else
+      //  doorTexture = null;
+      //EditorGUILayout.EndHorizontal();
+      // find material by windowtexture name
+    }
+
+    // balcony
+    balconyFoldout = EditorGUILayout.Foldout(balconyFoldout, "Balcony");
+    if (balconyFoldout)
+    {
+      // width
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Width");
+      setBalconyWidth = EditorGUILayout.Toggle(setBalconyWidth);
+      if (setBalconyWidth)
+        balconyWidth = EditorGUILayout.FloatField(balconyWidth, GUILayout.Width(30));
+      else
+        balconyWidth = 0f;
+      EditorGUILayout.EndHorizontal();
+      _building.balconyWidth = balconyWidth;
+
+      // height
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Height");
+      setBalconyHeight = EditorGUILayout.Toggle(setBalconyHeight);
+      if (setBalconyHeight)
+        balconyHeight = EditorGUILayout.FloatField(balconyHeight, GUILayout.Width(30));
+      else
+        balconyHeight = 0f;
+      EditorGUILayout.EndHorizontal();
+      _building.balconyHeight = balconyHeight;
+    }
+  }
+
+  private void RoofParams ()
+  {
+    EditorGUILayout.LabelField("Roof", EditorStyles.boldLabel);
+
+    rtype = (myRType) EditorGUILayout.EnumPopup("Roof type", rtype);
+    switch (rtype)
+    {
+      case myRType.Flat:
+        _building.roofType = Type.GetType(GetFullType(myRType.Flat));
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Roof decor texture");
+        inputRoofDecor = EditorGUILayout.Toggle(inputRoofDecor);
+        if (inputRoofDecor)
+          roofDecorTexture = EditorGUILayout.ObjectField(roofDecorTexture,
+                                                         typeof(Texture),
+                                                         false,
+                                                         GUILayout.Height(55));
+        else
+        {
+          _building.roofDecorMaterial = null;
+          roofDecorTexture = null;
+        }
+        EditorGUILayout.EndHorizontal();
+        // TODO find zhe material here
+        break;
+
+      case myRType.SinglePeak:
+        _building.roofType = Type.GetType(GetFullType(myRType.SinglePeak));
+        inputRoofDecor = false;
+        _building.roofDecorMaterial = null;
+        break;
+
+      case myRType.DoublePeak:
+        _building.roofType = Type.GetType(GetFullType(myRType.DoublePeak));
+        inputRoofDecor = false;
+        _building.roofDecorMaterial = null;
+        break;
+
+      default:
+        _building.roofType = null;
+        inputRoofDecor = false;
+        _building.roofDecorMaterial = null;
+        break;
+    }
+
+    // roof texture
+    EditorGUILayout.BeginHorizontal();
+    EditorGUILayout.LabelField("Roof texture");
+    inputRoof = EditorGUILayout.Toggle(inputRoof);
+    if (inputRoof)
+      roofTexture = EditorGUILayout.ObjectField(roofTexture,
+                                                typeof(Texture),
+                                                false,
+                                                GUILayout.Height(55));
+    else
+    {
+      _building.roofMaterial = null;
+      roofTexture = null;
+    }
+    EditorGUILayout.EndHorizontal();
+    // TODO find zhe material here
+
+    // roof base texture
+    EditorGUILayout.BeginHorizontal();
+    EditorGUILayout.LabelField("Roof base texture");
+    inputRoofBase = EditorGUILayout.Toggle(inputRoofBase);
+    if (inputRoofBase)
+      roofBaseTexture = EditorGUILayout.ObjectField(roofBaseTexture,
+                                                    typeof(Texture),
+                                                    false,
+                                                    GUILayout.Height(55));
+    else
+    {
+      _building.roofBaseMaterial = null;
+      roofBaseTexture = null;
+    }
+    EditorGUILayout.EndHorizontal();
+    // TODO find zhe material here
+  }
+
+  private string GetFullType (myRType t)
+  {
+    return "Thesis." + t.ToString() + "Roof,Assembly-CSharp-firstpass";
   }
 }
